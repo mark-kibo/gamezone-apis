2b87409bb477154ff18ba6922585e87e7961c2d2
mark_kibo <kibochamark@gmail.com>
2023-09-05 14:01:22 +0300

    Build:creted starup files

---
+++ accounts/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+import uuid
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class Profile(models.Model):
+    COUNTRY_CHOICES = [
+        ('KE', 'Kenya'),
+        ('TZ', 'Tanzania'),
+        ('UGD', 'Uganda'),
+    ]
+    
+    country = models.CharField(max_length=3, choices=COUNTRY_CHOICES)
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    location = models.TextField(max_length=255)
+    profile_pic = models.ImageField(upload_to="media")
+
+    def __str__(self):
+        return str(self.id)
+
+
+class GameZoneUser(AbstractUser):
+    
+    # Add any additional fields you need
+    profile=models.ForeignKey(Profile, on_delete=models.CASCADE, )
