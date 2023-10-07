6a703dc9612a4a3d41e6fbd742d54945c41d9aca
mark_kibo <kibochamark@gmail.com>
2023-10-07 13:44:06 +0300

    made changes on serializers file

--- new/serializers.py
+++ new/serializers.py
@@ -1,41 +1,41 @@
-# from rest_framework import serializers
-# from .models import Product, Sale, Loss
+from rest_framework import serializers
+from .models import Product, Sale, Loss
 
 
 
 
-# class ProductSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model=Product
-#         fields=[
-#             'id',
-#             'name',
-#             'category',
-#             'description',
-#             'price',
-#             'quantity'
-#         ]
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Product
+        fields=[
+            'id',
+            'name',
+            'category',
+            'description',
+            'price',
+            'quantity'
+        ]
     
-# class SalesSerializer(serializers.ModelSerializer):
-#     product_name=serializers.SerializerMethodField(read_only=True)
-#     class Meta:
-#         model=Sale
-#         fields=[   
-#             'id',
-#             'product_name',
-#             "quantity_sold",
-#             "sale_price",
-#             'sale_date'
-#         ]
+class SalesSerializer(serializers.ModelSerializer):
+    product_name=serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model=Sale
+        fields=[   
+            'id',
+            'product_name',
+            "quantity_sold",
+            "sale_price",
+            'sale_date'
+        ]
 
 
 
-#     def get_product_name(self, instance):
-#         return instance.product.name
+    def get_product_name(self, instance):
+        return instance.product.name
 
         
-# class LossSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model=Loss
-#         fields="__all__"
+class LossSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Loss
+        fields="__all__"
 
