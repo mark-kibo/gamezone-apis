c0db235f21bd4db6f2e37907108ed3b27cd0ceea
mark_kibo <kibochamark@gmail.com>
2023-10-07 13:39:41 +0300

    uncommented things

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,41 +1,41 @@
-# from rest_framework.serializers import ModelSerializer,CharField
-# from .models import Profile, GameZoneUser
-# from django.contrib.auth.hashers import make_password
+from rest_framework.serializers import ModelSerializer,CharField
+from .models import Profile, GameZoneUser
+from django.contrib.auth.hashers import make_password
 
 
-# class GamezoneUserSerializer(ModelSerializer):
+class GamezoneUserSerializer(ModelSerializer):
     
-#     password =CharField(
-#         write_only=True,
-#         required=True,
-#         help_text='Leave empty if no change needed',
-#         style={'input_type': 'password', 'placeholder': 'Password'}
-#     )
-#     class Meta:
-#         model=GameZoneUser
-#         fields=[
-#            'id','username', 'email', 'password', 'profile'
-#         ]
-#         read_only_fields = ['profile']
+    password =CharField(
+        write_only=True,
+        required=True,
+        help_text='Leave empty if no change needed',
+        style={'input_type': 'password', 'placeholder': 'Password'}
+    )
+    class Meta:
+        model=GameZoneUser
+        fields=[
+           'id','username', 'email', 'password', 'profile'
+        ]
+        read_only_fields = ['profile']
 
 
-#     def create(self, validated_data):
-#         validated_data['password']=make_password(validated_data.get("password"))
-#         return super().create(validated_data)
+    def create(self, validated_data):
+        validated_data['password']=make_password(validated_data.get("password"))
+        return super().create(validated_data)
     
-#     def update(self, instance, validated_data):
-#         pass6=validated_data['password']
-#         print(pass6)
-#         validated_data['password']=make_password(pass6)
-#         print( validated_data['password'])
-#         return super().update(instance, validated_data)
+    def update(self, instance, validated_data):
+        pass6=validated_data['password']
+        print(pass6)
+        validated_data['password']=make_password(pass6)
+        print( validated_data['password'])
+        return super().update(instance, validated_data)
     
     
     
 
-# class ProfileSeriaLizer(ModelSerializer):
-#     class Meta:
-#         model=Profile
-#         fields=["id", "country", "location", "profile_pic"]
+class ProfileSeriaLizer(ModelSerializer):
+    class Meta:
+        model=Profile
+        fields=["id", "country", "location", "profile_pic"]
     
    
--- authapp/views.py
+++ authapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-# from new.models import Product, Sale, Loss
-# from accounts.models import GameZoneUser
+from new.models import Product, Sale, Loss
+from accounts.models import GameZoneUser
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from django.forms.models import model_to_dict
@@ -28,33 +28,32 @@ class MyTokenObtainPairView(TokenObtainPairView):
 
 
 class ProductAggregateAPIView(ViewSet):
-    pass
-#     queryset=Product.objects.all()
-#     def get_overview(self, request, *args, **kwargs):
-#         # Calculate the aggregate (e.g., sum of all product prices)
-#         aggregate_result =self.queryset.aggregate(total_price=Sum('price'))
-#         total_users=GameZoneUser.objects.count()
-#         total_products=Product.objects.count()
-#         total_sales_revenue=Sale.objects.aggregate(total_price=Sum('sale_price'))
+    queryset=Product.objects.all()
+    def get_overview(self, request, *args, **kwargs):
+        # Calculate the aggregate (e.g., sum of all product prices)
+        aggregate_result =self.queryset.aggregate(total_price=Sum('price'))
+        total_users=GameZoneUser.objects.count()
+        total_products=Product.objects.count()
+        total_sales_revenue=Sale.objects.aggregate(total_price=Sum('sale_price'))
        
-#     #    get profit earned
-#         total_profit = Sale.objects.aggregate(total_profit=Sum('product__price'))['total_profit']
-#         total_profit = int(total_sales_revenue["total_price"])- int(total_profit)
-#         top_products_by_quantity = Product.objects.annotate(total_quantity_sold=Sum('quantity')).order_by('total_quantity_sold')[:5]
+    #    get profit earned
+        total_profit = Sale.objects.aggregate(total_profit=Sum('product__price'))['total_profit']
+        total_profit = int(total_sales_revenue["total_price"])- int(total_profit)
+        top_products_by_quantity = Product.objects.annotate(total_quantity_sold=Sum('quantity')).order_by('total_quantity_sold')[:5]
 
-#         # //losses
-#         total_loss_incurred=Loss.objects.aggregate(total_loss=Sum('loss_amount'))['total_loss']
-# # If you want to handle cases where there are no sales, you can provide a default value
-#         if total_profit is None:
-#             total_profit = 0
+        # //losses
+        total_loss_incurred=Loss.objects.aggregate(total_loss=Sum('loss_amount'))['total_loss']
+# If you want to handle cases where there are no sales, you can provide a default value
+        if total_profit is None:
+            total_profit = 0
 
-#         return Response(
-#             {
-#                 "total_loss": total_loss_incurred,
-#                 "total_revenue": total_sales_revenue["total_price"],
-#                 "profit" : total_profit,
-#             "aggregate_results": aggregate_result,
-#              "totalusers":  total_users,
-#              "total_products": total_products}
-#         )
-#     permission_classes=[IsAuthenticatedOrReadOnly]
+        return Response(
+            {
+                "total_loss": total_loss_incurred,
+                "total_revenue": total_sales_revenue["total_price"],
+                "profit" : total_profit,
+            "aggregate_results": aggregate_result,
+             "totalusers":  total_users,
+             "total_products": total_products}
+        )
+    permission_classes=[IsAuthenticatedOrReadOnly]
