b3a829ef4f216be9c6b40ef88998d9954bf14b3f
mark_kibo <kibochamark@gmail.com>
2023-10-07 17:50:38 +0300

    updates

--- expenses/views.py
+++ expenses/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from .models import Expense
 from .serializers import ExpenseSerializer
+from django.views.decorators.cache import never_cache
 from django.utils.decorators import method_decorator
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 # Create your views here.
